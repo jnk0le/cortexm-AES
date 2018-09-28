ac53789c08acf9f583079be373e066a1fcc0c220
jnk0le <jnk0le@hotmail.com>
2018-09-28 02:04:52 +0200

    added optimized ctr implementation for cortex m7

---
+++ target/AES_CM7_modes.h
@@ -0,0 +1,41 @@
+/*!
+ * \file AES_CM7_modes.h
+ * \version 3.7.0
+ * \brief SP 800-38A compliant, software AES block modes implementations optimized for cortex-m3/4
+ *
+ * ctr encryption can be recycled for decryption thus requiring only 1K T table for encryption
+ *
+ * This part is based on CM34 implementation, carefully reordered for dual issue pipeline, with 2x32 bit DTCM
+ * interface, to avoid data dependent stalls when two neighbouring loads both accesses data on even/odd location
+ *
+ * \author jnk0le <jnk0le@hotmail.com>
+ * \copyright MIT License
+ * \date Aug 2018
+ */
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+	extern "C" {
+#endif
+
+
+	// currently requires pointer to ctx struct in form:
+
+	//typedef struct {
+	//    uint8_t nonce[16];
+	//    uint8_t rk[(n+1)*16];
+	//} ctx;
+
+
+	void CM7_1T_AES_CTR_enc(void* ctx, const uint8_t* data_in, uint8_t* data_out, uint32_t rounds, uint32_t blocks_cnt);
+
+	//void CM34_1T_AES_128_CTR_enc_unrolled(void* ctx, const uint8_t* data_in, uint8_t* data_out, uint32_t blocks_cnt);
+	//void CM34_1T_AES_192_CTR_enc_unrolled(void* ctx, const uint8_t* data_in, uint8_t* data_out, uint32_t blocks_cnt);
+	//void CM34_1T_AES_256_CTR_enc_unrolled(void* ctx, const uint8_t* data_in, uint8_t* data_out, uint32_t blocks_cnt);
+
+
+
+#ifdef __cplusplus
+	}
+#endif
--- target/modes_impl.hpp
+++ target/modes_impl.hpp
@@ -1,6 +1,6 @@
 /*!
  * \file modes_impl.hpp
- * \version 3.6.0
+ * \version 3.7.0
  * \brief wrappers for block mode ciphers
  *
  * \author jnk0le <jnk0le@hotmail.com>
@@ -11,6 +11,7 @@
 #include <stdint.h>
 
 #include "AES_CM34_modes.h"
+#include "AES_CM7_modes.h"
 
 namespace aes
 {
@@ -145,6 +146,19 @@ namespace target
 					break;
 				}
 
+			}
+		};
+
+	template<size_t key_length, template<size_t> class base_impl>
+		class CTR_CM7_1T : private CipherContext<key_length, base_impl>
+		{
+		public:
+			using CipherContext<key_length, base_impl>::setEncKey;
+
+			void encrypt(const uint8_t* data_in, uint8_t* data_out, void* nonce, uint32_t blocks_cnt)
+			{
+				//nonce should be placed right before expanded key
+				CM7_1T_AES_CTR_enc(nonce, data_in, data_out, this->key_rounds, blocks_cnt);
 			}
 		};
 
