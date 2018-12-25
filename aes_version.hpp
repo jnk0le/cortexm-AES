0f9629c0a566ba8db68db4d9b9f505ef57363224
jnk0le <jnk0le@hotmail.com>
2018-12-26 00:19:29 +0100

    added version header

---
+++ aes_version.hpp
@@ -0,0 +1,25 @@
+/*!
+ * \file aes_version.hpp
+ * \brief holds the current library version
+ *
+ * \author jnk0le <jnk0le@hotmail.com>
+ * \copyright MIT License
+ * \date Dec 2018
+ */
+
+#ifndef AES_VERSION_HPP
+#define AES_VERSION_HPP
+
+namespace aes
+{
+namespace version
+{
+	//need to do it in somehow better way
+
+	constexpr int major = 0;
+	constexpr int minor = 9;
+	constexpr int patch = 0;
+}
+}
+
+#endif //AES_VERSION_HPP
